fc761071eb1170a7ac6cbc435b97a10097c5da9f
matlorr <matvey.lorkish@outlook.de>
2020-02-10 20:27:25 +0100

    initial commit

---
+++ src/main/java/de/matlorr/exp/lifedb/LifedbApplication.java
@@ -0,0 +1,13 @@
+package de.matlorr.exp.lifedb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LifedbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LifedbApplication.class, args);
+    }
+
+}
---
+++ src/test/java/de/matlorr/exp/lifedb/LifedbApplicationTests.java
@@ -0,0 +1,13 @@
+package de.matlorr.exp.lifedb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LifedbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
